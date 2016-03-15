56ba3ba1f7348e039641940f913e5f1955eadb4f
suixinqing888 <suixinqing888@gmail.com>
2016-03-14 17:25:04 -0700

    copy files

--- ApacheHelpers/js/elven-help.js
+++ ApacheHelpers/js/elven-help.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {  
-    $('table').addClass('table table-striped table-hover');  
+
+$(document).ready(function() {
+    $('table').addClass('table table-striped table-hover');
+
+    $('#getPresidents').click(getPresidents);
+
+    function getPresidents() {
+        $.getJSON('/cgi-bin/get-presidents.py', function(presidents) {
+            presidents.forEach(function(president) {
+                $('#bar').append('<li>' + president[1] + ' ' + president[2] + '</li>');
+            });
+        })
+    }
 });
